c00ef2c706cd55533932b23a5adea9beed15fed2
chikaasan <chyngyz.asanhodjoevv@gmail.com>
2021-03-17 19:52:18 +0300

    Added commit

--- lesson4-2.dart
+++ lesson4-2.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-main() { //a program that asks for a name and age
+main() {  //a program that asks for a name and age
   stdout.write("What's your name?");
   String name = stdin.readLineSync()!;
   stdout.write("How old are you?");
--- lesson4-4.dart
+++ lesson4-4.dart
@@ -1,5 +1,5 @@
 
-main() { //a program that prints less than five
+main() { //a program that prints less than  five
 var array = [4, 7, 8, 3, 2, 9, 1, 6, 5];
 print("${array[0]} ${array[3]} ${array[4]} ${array[6]}");
 
